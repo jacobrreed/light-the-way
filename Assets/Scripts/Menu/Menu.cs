da6f714a110251a25861eb5df82c8423f0e87753
jacobrreed <jacob.russell.reed@gmail.com>
2024-09-15 14:08:17 -0400

    Created start menu and build settings for itch io

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Joystick1Button0))
+    {
+      OnStart();
+    }
+  }
+
+  public void OnStart()
+  {
+    SceneManager.LoadScene("Level1");
+  }
+}
